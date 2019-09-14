32550e118d50710d03fb21c796820a770b2e9a29
eddunic <eddunic@gmail.com>
2019-09-14 19:45:47 -0400

    alteração classes, cadastro e main

--- app/src/main/java/com/example/adopets/activity/CadastroActivity.kt
+++ app/src/main/java/com/example/adopets/activity/CadastroActivity.kt
@@ -1,13 +1,103 @@
 package com.example.adopets.activity
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import com.example.adopets.R
+import com.example.adopets.model.Usuario
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.*
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_cadastro.*
+import java.util.*
 
 class CadastroActivity : AppCompatActivity() {
+
+    private lateinit var database : DatabaseReference
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cadastro)
+        supportActionBar?.hide()
+
+        database = FirebaseDatabase.getInstance().reference
+        auth = FirebaseAuth.getInstance()
     }
-}
+
+    fun cadastrar(view: View) {
+
+        var nome = editTextNome.text.toString()
+        var email = editTextEmail.text.toString()
+        var senha = editTextSenha.text.toString()
+
+        var valido = true
+
+        if (nome.isEmpty()) {
+            editTextNome.error = "Campo obrigatório!"
+            valido = false
+        }
+
+        if (email.isEmpty()) {
+            editTextEmail.error = "Campo obrigatório!"
+            valido = false
+        }
+
+        if (senha.isEmpty()) {
+            editTextSenha.error = "Campo obrigatório!"
+            valido = false
+        }
+
+        if (valido) {
+            val usuarios = database.child("usuarios")
+            val u = Usuario()
+            u.email = email
+            u.nome = nome
+            val ref = usuarios.push()
+
+            auth.createUserWithEmailAndPassword(u.email, senha)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        ref.child("email").setValue(u.email)
+                        ref.child("nome").setValue(u.nome)
+                        startActivity(Intent(applicationContext, MainActivity::class.java))
+                        Toast.makeText(this, "Usuário cadastrado com sucesso!", Toast.LENGTH_SHORT)
+                            .show()
+                    } else {
+                        try {
+                            throw task.exception!!
+                        } catch (e: FirebaseAuthWeakPasswordException) {
+                            Toast.makeText(
+                                this@CadastroActivity,
+                                "Senha fraca!",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        } catch (e: FirebaseAuthInvalidCredentialsException) {
+                            Toast.makeText(
+                                this@CadastroActivity,
+                                "Email inválido!",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        } catch (e: FirebaseAuthUserCollisionException) {
+                            Toast.makeText(
+                                this@CadastroActivity,
+                                "Usuário já cadastrado!",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        } catch (e: Exception) {
+                            Toast.makeText(
+                                this@CadastroActivity,
+                                "" + e.message,
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+
--- app/src/main/java/com/example/adopets/model/classes.kt
+++ app/src/main/java/com/example/adopets/model/classes.kt
@@ -1,12 +1,14 @@
 package com.example.adopets.model
 
+import java.util.*
+
 open class Usuario(
     open var email: String = "",
-    open var senha: String = "",
-    open var dataNasc: String = "",
-    open var foto: String = "",
-    open var telefone: String = "",
     open var nome: String = "",
+    open var senha: String = "",
+    open var dataNasc: Date = Date(),
+    open var foto: ByteArray = "".toByteArray(),
+    open var telefone: String = "",
     open var bairro: String = "",
     open var rua: String = "",
     open var numero: String = "",
@@ -14,60 +16,61 @@ open class Usuario(
     open var cep: String = ""
 )
 
-data class Doador(override var email: String = "", override var senha: String = "",
-             override var dataNasc: String = "", override var foto: String = "",
-             override var telefone: String = "", override var nome: String = "", override var bairro: String = "",
+class Doador(override var email: String = "", override var nome: String = "",
+             override var senha: String = "", override var dataNasc: Date = Date(),
+             override var foto: ByteArray = "".toByteArray(), override var telefone: String = "", override var bairro: String = "",
              override var rua: String = "", override var numero: String = "", override var complemento: String = "",
-             override var cep: String = ""): Usuario(email, senha, dataNasc, foto, telefone, nome, bairro, rua, numero, complemento, cep)
+             override var cep: String = ""): Usuario(email, nome, senha, dataNasc, foto, telefone, bairro, rua, numero, complemento, cep)
 
-data class Adotante(override var email: String = "", override var senha: String = "",
-               override var dataNasc: String = "", override var foto: String = "",
-               override var telefone: String = "", override var nome: String = "", override var bairro: String = "",
-               override var rua: String = "", override var numero: String = "", override var complemento: String = "", override var cep: String = ""): Usuario(email, senha, dataNasc, foto, telefone, nome, bairro, rua, numero, complemento, cep)
+class Adotante(override var email: String = "", override var nome: String = "",
+               override var senha: String = "", override var dataNasc: Date = Date(),
+               override var foto: ByteArray = "".toByteArray(), override var telefone: String = "", override var bairro: String = "",
+               override var rua: String = "", override var numero: String = "", override var complemento: String = "",
+               override var cep: String = ""): Usuario(email, nome, senha, dataNasc, foto, telefone, bairro, rua, numero, complemento, cep)
 
-data class Voluntario(override var email: String = "", override var senha: String = "",
-                 override var dataNasc: String = "", override var foto: String = "",
-                 override var telefone: String = "", override var nome: String = "", override var bairro: String = "",
+class Voluntario(override var email: String = "", override var nome: String = "",
+                 override var senha: String = "", override var dataNasc: Date = Date(),
+                 override var foto: ByteArray = "".toByteArray(), override var telefone: String = "", override var bairro: String = "",
                  override var rua: String = "", override var numero: String = "", override var complemento: String = "",
-                 override var cep: String = "", var pontuacao: Float = 0F): Usuario(email, senha, dataNasc, foto, telefone, nome, bairro, rua, numero, complemento, cep)
+                 override var cep: String = "", var pontuacao: Float = 0F): Usuario(email, nome, senha,dataNasc, foto, telefone, bairro, rua, numero, complemento, cep)
 
-data class Contratante(override var email: String = "", override var senha: String = "",
-                  override var dataNasc: String = "", override var foto: String = "",
-                  override var telefone: String = "", override var nome: String = "", override var bairro: String = "",
+class Contratante(override var email: String = "", override var nome: String = "",
+                  override var senha: String = "", override var dataNasc: Date = Date(),
+                  override var foto: ByteArray = "".toByteArray(), override var telefone: String = "", override var bairro: String = "",
                   override var rua: String = "", override var numero: String = "", override var complemento: String = "",
-                  override var cep: String = ""): Usuario(email, senha, dataNasc, foto, telefone, nome, bairro, rua, numero, complemento, cep)
+                  override var cep: String = ""): Usuario(email, nome, senha, dataNasc, foto, telefone, bairro, rua, numero, complemento, cep)
 
-data class Processo(
-    var codigo: Long = 0L,
+class Processo(
+    var id: String = "",
     var motivo: String = "",
     var situacao: String = "",
-    var dataCriacao: String = "",
-    var dataFim: String = "",
+    var dataCriacao: Date = Date(),
+    var dataFim: Date = Date(),
     var doador: Doador = Doador()
 )
 
-data class AdotanteProcesso(
+class AdotanteProcesso(
     var adotante: Adotante = Adotante(),
     var processo: Processo = Processo()
 )
 
-data class Servico(
-    var codigo: Long = 0L,
+class Servico(
+    var id: String = "",
     var tipo: String = "",
-    var dataInicio: String = "",
-    var dataFim: String = "",
+    var dataInicio: Date = Date(),
+    var dataFim: Date = Date(),
     var descricao: String = "",
     var voluntario: Voluntario = Voluntario()
 )
 
-data class ContratanteServico(
+class ContratanteServico(
     var contratante: Contratante = Contratante(),
     var servico: Servico = Servico()
 )
 
-data class Animal(
-    var codigo: Long = 0L,
-    var foto: String = "",
+class Animal(
+    var id: String = "",
+    var foto: ByteArray = "".toByteArray(),
     var situacao: String = "",
     var raca: String = "",
     var descricao: String = "",
@@ -76,15 +79,15 @@ data class Animal(
     var necessidade: String = "",
     var tipo: String = "",
     var nome: String = "",
-    var dataNasc: String = ""
+    var dataNasc: Date = Date()
 )
 
-data class AnimalProceso(
+class AnimalProceso(
     var animal: Animal = Animal(),
     var processo: Processo = Processo()
 )
 
-data class AnimalServico(
+class AnimalServico(
     var animal: Animal = Animal(),
     var servico: Servico = Servico()
 )
