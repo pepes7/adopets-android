a98881ccdd5e2f52f57720f221c4a7c99d994673
eddunic <eddunic@gmail.com>
2019-09-15 16:28:43 -0400

    maps activity

--- app/src/main/java/com/example/adopets/fragment/MapaFragment.kt
+++ app/src/main/java/com/example/adopets/fragment/MapaFragment.kt
@@ -6,8 +6,18 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.adopets.R
-
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import android.graphics.Bitmap
+import android.support.v4.content.ContextCompat
+import com.google.android.gms.maps.model.BitmapDescriptor
+import android.content.Context
+import android.graphics.Canvas
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -25,8 +35,64 @@ class MapaFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mapa, container, false)
+        val rootView = inflater.inflate(com.example.adopets.R.layout.fragment_mapa, container, false)
+
+        val mapFragment =
+            childFragmentManager.findFragmentById(com.example.adopets.R.id.frg) as SupportMapFragment?  //use SuppoprtMapFragment for using in fragment instead of activity  MapFragment = activity   SupportMapFragment = fragment
+        mapFragment!!.getMapAsync { mMap ->
+            mMap.mapType = GoogleMap.MAP_TYPE_NORMAL
+
+            mMap.clear() //clear old markers
+
+            val lugar = CameraPosition.builder()
+                .target(LatLng(-3.074601, -60.039474))
+                .zoom(10f)
+                .bearing(0f)
+                .tilt(45f)
+                .build()
+
+            mMap.animateCamera(CameraUpdateFactory.newCameraPosition(lugar), 10000, null)
+
+            mMap.addMarker(
+                MarkerOptions()
+                    .position(LatLng(-3.074601, -60.039474))
+                    .title("Mel")
+                    .icon(this.context?.let{bitmapDescriptorFromVector(it, com.example.adopets.R.drawable.mel_perfil)})
+            )
+
+            mMap.addMarker(
+                MarkerOptions()
+                    .position(LatLng(37.4629101, -122.2449094))
+                    .title("Iron Man")
+                    .snippet("His Talent : Plenty of money")
+            )
+
+            mMap.addMarker(
+                MarkerOptions()
+                    .position(LatLng(37.3092293, -122.1136845))
+                    .title("Captain America")
+            )
+        }
+
+        return rootView
     }
 
+    private fun bitmapDescriptorFromVector(context: Context, vectorResId: Int): BitmapDescriptor {
+        val vectorDrawable = ContextCompat.getDrawable(context, vectorResId)
+        vectorDrawable!!.setBounds(
+            0,
+            0,
+            vectorDrawable.intrinsicWidth,
+            vectorDrawable.intrinsicHeight
+        )
+        val bitmap = Bitmap.createBitmap(
+            vectorDrawable.intrinsicWidth,
+            vectorDrawable.intrinsicHeight,
+            Bitmap.Config.ARGB_8888
+        )
+        val canvas = Canvas(bitmap)
+        vectorDrawable.draw(canvas)
+        return BitmapDescriptorFactory.fromBitmap(bitmap)
+    }
 
 }
