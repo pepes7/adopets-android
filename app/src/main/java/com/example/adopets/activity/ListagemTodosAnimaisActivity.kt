4beaab13ec15c2c382f03da2618e977f604f497d
eddunic <eddunic@gmail.com>
2019-11-18 17:35:19 -0400

    visualização animal grid2 - gabriel

--- app/src/main/java/com/example/adopets/activity/ListagemTodosAnimaisActivity.kt
+++ app/src/main/java/com/example/adopets/activity/ListagemTodosAnimaisActivity.kt
@@ -11,17 +11,21 @@ import com.example.adopets.R
 import com.example.adopets.adapter.AnimalAdapter
 import com.example.adopets.adapter.AnimalAdapter2
 import com.example.adopets.model.Animal
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
 import de.hdodenhof.circleimageview.CircleImageView
 import kotlinx.android.synthetic.main.activity_listagem_todos_animais.*
 import kotlinx.android.synthetic.main.list_view_animais.*
 import kotlinx.android.synthetic.main.list_view_animais2.*
+import java.util.*
 
 class ListagemTodosAnimaisActivity : AppCompatActivity() {
 
-    private lateinit var recyclerView: RecyclerView
-    private lateinit var recyclerViewTodosAnimais: RecyclerView
+    private var animais = arrayListOf<Animal>()
+    private lateinit var adapterAnimal: AnimalAdapter2
     private lateinit var btn_ok: Button
+    private lateinit var animaisRecuperados : DatabaseReference
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,13 +43,48 @@ class ListagemTodosAnimaisActivity : AppCompatActivity() {
             startActivity(Intent(this, PerfilAnimalDoacaoActivity::class.java))
         }
 
-        recyclerView = findViewById(R.id.recyclerViewTodosAnimais)
-        recyclerViewTodosAnimais = findViewById(R.id.recyclerViewTodosAnimais)
+        auth = FirebaseAuth.getInstance()
+        animaisRecuperados =  FirebaseDatabase.getInstance().reference.child("animal")
 
-        recyclerView = recyclerViewTodosAnimais
-        recyclerView.adapter = AnimalAdapter2(animais(), this)
-        val layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
-        recyclerView.layoutManager = layoutManager
+
+
+        recyclerViewTodosAnimais.layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+        recyclerViewTodosAnimais.hasFixedSize()
+
+        adapterAnimal = AnimalAdapter2(this,animais)
+
+        recyclerViewTodosAnimais.adapter = adapterAnimal
+
+        //recupera dados
+        recuperarAnimal()
+
+
+    }
+
+    private fun recuperarAnimal(){
+        animaisRecuperados.addValueEventListener(object : ValueEventListener{
+            override fun onCancelled(p0: DatabaseError) {
+            }
+
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                animais.clear()
+                for (d in dataSnapshot.children){
+                    val u = d.getValue(Animal::class.java)
+
+                    //verifica se o usuário atual fez a doaçaõ
+                    if(!u!!.doador.equals(auth!!.currentUser!!.uid)){
+                        animais.add(u!!)
+                    }
+
+
+                }
+
+                Collections.reverse(animais)
+                adapterAnimal.notifyDataSetChanged()
+            }
+
+        })
+
     }
 
     private fun animais(): List<Animal> {
